9dee873dc64a015289f1445f808e6f6cc18e819e
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-25 18:16:18 +0530

    uc5

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,34 @@
 package com.bridgelabz;
+import java.util.*;
 
 public class EmpWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation program");
+        System.out.println("Welcome to Employee Wage Computation program on Master Branch");
+
+        Random randNum = new Random();
+
+        int hour = 0, sum = 0;
+
+        for (int i = 0; i < 20; i++) {
+
+            int check = randNum.nextInt(3);
+
+            switch(check){
+                case 0 :
+                    hour=0;
+                    break;
+                case 1 :
+                    hour=4;
+                    break;
+                default :
+                    hour=8;
+            }
+
+            System.out.println("daily Wages are : "+hour*20);
+            sum+=hour*20;
+        }
+
+        System.out.println("Total Wages in 20 day period "+sum);
+
     }
 }
