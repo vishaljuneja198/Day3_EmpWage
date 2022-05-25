17a9c492dc566eba44da251211ccb37fbe52634f
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-25 17:33:44 +0530

    uc1 branch

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
+import java.util.*;
 
-public class EmpWageComputation {
+public class EmployeeWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation program");
+        System.out.println("Welcome to Employee Wage Computation program on Master Branch");
+        Random randNum = new Random();
+        int check = randNum.nextInt(2);
+        if (check == 0)
+            System.out.println("Employee is Absent");
+        else
+            System.out.println("Employee is Present");
     }
 }
